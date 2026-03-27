37be376e569e76e4ae0d22a3fec569c032942fcd
zzhi123 <zezhii@foxmail.com>
2026-03-27 09:55:55 +0800

    Configure GitHub Pages deployment

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "zzhi123.github.io/QuartzBlog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
